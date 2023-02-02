bab9b9a9bfe1c59140dbedd78fb046113a5d9375
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-01 20:21:17 -0600

    I made the logical basis of the operation.

---
+++ percentages/percentages.js
@@ -0,0 +1,15 @@
+const inputPrice = document.querySelector('.input-producto');
+const inputDiscount = document.querySelector('.input-descuento');
+const btn = document.querySelector('.button');
+const pResult = document.querySelector('.result');
+
+btn.addEventListener('click', calcPriceDiscount);
+
+function calcPriceDiscount() {
+    const price = inputPrice.value;
+    const discount = inputDiscount.value;
+    const priceDiscount = (price - ((price * discount)/ 100));
+    console.log(priceDiscount);
+
+    pResult.innerText = '$' + priceDiscount;
+}
