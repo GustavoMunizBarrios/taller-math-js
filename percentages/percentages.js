ef1d1ee70a700658f7abd560dad7b58ef5c2aea1
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-02 09:30:14 -0600

    I added the .toFixed(2) method to the priceDiscount result to display only two decimal places in the result.

--- percentages/percentages.js
+++ percentages/percentages.js
@@ -2,14 +2,29 @@ const inputPrice = document.querySelector('.input-producto');
 const inputDiscount = document.querySelector('.input-descuento');
 const btn = document.querySelector('.button');
 const pResult = document.querySelector('.result');
+const pError = document.querySelector('.error');
 
 btn.addEventListener('click', calcPriceDiscount);
 
 function calcPriceDiscount() {
-    const price = inputPrice.value;
-    const discount = inputDiscount.value;
-    const priceDiscount = (price - ((price * discount)/ 100));
-    console.log(priceDiscount);
+    const price = Number(inputPrice.value); //Number convierte el string inputPrice en un number.
+    const discount = Number(inputDiscount.value);//Number convierte el string inputDiscount en un number.
 
-    pResult.innerText = '$ ' + priceDiscount;
+    if (!price || !discount) { //Si no hay price o discount:
+        pError.innerText = 'Por favor escriba el precio y descuento del producto';
+        pResult.innerText = '';
+    } else {
+        if (discount > 100) {
+            pError.innerText = 'Escriba un porcentaje entre 1 y 100'
+            pResult.innerText = '';
+            return;
+        }
+        const priceDiscount = (price - ((price * discount)/ 100)).toFixed(2);
+        console.log(priceDiscount);
+    
+        pResult.innerText = '$ ' + priceDiscount;
+        pError.innerText = '';
+    }
+
+
 }
