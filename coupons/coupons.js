cd04e6c07a9bb8931dffa8aa2146277e7f0d58f2
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-02 09:51:44 -0600

    I added the coupons folder with the 3 main files.

---
+++ coupons/coupons.js
@@ -0,0 +1,45 @@
+const inputPrice = document.querySelector('#price');
+const inputCoupon = document.querySelector('#coupon');
+const btn = document.querySelector('#calcular');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener('click', calcularPrecioConDescuento);
+
+const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+
+function calcularPrecioConDescuento() {
+  const price = Number(inputPrice.value);
+  const coupon = inputCoupon.value;
+
+  if (!price || !coupon) {
+    pResult.innerText = 'CHANCLA por favor llena el formulario';
+    return;
+  }
+  
+  let discount;
+
+  switch (coupon) {
+    case 'Cupon del 30%':
+      discount = 30;
+      break;
+    case 'Cupon del 25%':
+      discount = 25;
+      break;
+    default:
+      pResult.innerText = 'El cupón no es válido';
+      return;
+  }
+  
+  // if (coupon == 'JuanDC_es_Batman') {
+  //   discount = 30;
+  // } else if (coupon == 'no_le_digas_a_nadie') {
+  //   discount = 25;
+  // } else {
+  // pResult.innerText = 'El cupón no es válido';
+  // return;
+  // }
+  
+  const newPrice = (price * (100 - discount)) / 100;
+
+  pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
+}
