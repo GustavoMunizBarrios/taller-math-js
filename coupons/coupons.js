45c289c96c14db45d863a3b07fec8d02d1b0637f
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-02 18:30:27 -0600

    I finished the logic of the coupons.js

--- coupons/coupons.js
+++ coupons/coupons.js
@@ -5,20 +5,55 @@ const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
-const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+//const arrayUObjecto = undefined; // ['cupones': descuento] {}?
+
+const couponList = [];
+couponList.push({
+    name: 'descuento20',
+    discount: 20,
+})
+couponList.push({
+    name: 'descuento30',
+    discount: 30,
+})
+couponList.push({
+    name: 'descuento40',
+    discount: 40,
+})
+couponList.push({
+    name: 'descuento50',
+    discount: 50,
+})
 
 function calcularPrecioConDescuento() {
   const price = Number(inputPrice.value);
   const coupon = inputCoupon.value;
 
   if (!price || !coupon) {
-    pResult.innerText = 'CHANCLA por favor llena el formulario';
+    pResult.innerText = 'Por favor llena el formulario';
     return;
   }
-  
+
+  function isCouponInArray(couponElement) {
+    return couponElement.name == coupon;
+  }
+  console.log(isCouponInArray);
+
+  //utilizamos el método .find para preguntarle al array couponList si alguno de los objetos con propiedad 'name' coincide con lo que escribieron los usuarios en inputCoupon
+  const couponInArray = couponList.find(isCouponInArray);
+  console.log(couponInArray);
+
   let discount;
 
-  switch (coupon) {
+  if (couponInArray) {
+    discount = couponInArray.discount;
+  } else {
+    pResult.innerText = "El cupón no es válido.";
+    return;
+  }
+
+
+/*   switch (coupon) {
     case 'Cupon del 30%':
       discount = 30;
       break;
@@ -28,7 +63,7 @@ function calcularPrecioConDescuento() {
     default:
       pResult.innerText = 'El cupón no es válido';
       return;
-  }
+  } */
   
   // if (coupon == 'JuanDC_es_Batman') {
   //   discount = 30;
