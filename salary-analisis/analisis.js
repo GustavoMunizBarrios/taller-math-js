c052bb49d3fd4d6670129a0b3ae9beb6ad9a39ff
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-02-08 14:20:40 -0600

    I finished the function called medianaEmpresasYear

--- salary-analisis/analisis.js
+++ salary-analisis/analisis.js
@@ -51,3 +51,44 @@ function proyecciónPorPersona(nombrePersona) {
 
     return 'La proyección salarial de '+ nombrePersona +' '+ 'es de '+ nuevoSalario;
 }
+
+// Análisis empresarial
+
+/*empresas {
+    Industrias Mokepon: {
+        2018: [salarios, salarios, salarios]
+        2019: [salarios, salarios, salarios]
+        2020:
+        2021:
+    },
+    Industrias Mokepon: {},
+    Industrias Mokepon: {},
+    Industrias Mokepon: {},
+
+} */
+const empresas = {}; // Aqui guardamos toda la información de todas las empresas que tengamos
+for (persona of salarios) { //
+     for (trabajo of persona.trabajos) {
+        //console.log(trabajo);
+        if (!empresas[trabajo.empresa]) { // Si no existe trabajo.empresa en el objeto empresas.
+            empresas[trabajo.empresa] = {} // Si en efecto no existe, crea un objeto con esa información dentro.
+        }
+        if (!empresas[trabajo.empresa][trabajo.year]) { // Si trabajo.empresa no tiene la propiedad trabajo.year
+            empresas[trabajo.empresa][trabajo.year] = []; // Si en efecto no existe, crea un array 
+        }
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario)
+    } 
+}
+console.log({empresas});
+
+function medianaEmpresasYear(nombre, year) {
+    if (!empresas[nombre]) {
+        console.warn('La empresa no existe');
+
+    } else if (!empresas[nombre][year]) {
+        console.log('La empresa no dio salarios ese año');
+    } else {
+        result = MainMath.calcularMediana(empresas[nombre][year]);
+        return 'La mediana de salarios de la empresa ' + nombre + ' en el año ' + year + ' es de: ' + result;
+    }
+}
